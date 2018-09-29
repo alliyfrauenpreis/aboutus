c71b54e10f0e702d64e3e9052fdabca847563b78
Alliy Frauenpreis <alliyfrauenpreis@Alliys-MacBook-Pro.local>
2018-09-29 15:09:09 -0700

    Progress

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 
+	var permitted = true;
+
 	$('#thumbnails-container .thumbnail').click(function(){
-		animateNewThumbnail($(this));
-		slidePanes();
+		if (permitted) {
+			permitted = false;
+			animateNewThumbnail($(this));
+			slidePanes();
+		}
 
 	});
 
@@ -12,10 +17,11 @@ $(document).ready(function(){
 		var width = $('#panes-container #pane1 #thumb1').width();
 		var height = $('#panes-container #pane1 #thumb1').height();
 
-		console.log(position);
 		// Add placeholder
 
 		var currentPos = $(thumbnail).position();
+		var currentWidth = $(thumbnail).width();
+		var currentHeight = $(thumbnail).height();
 		var placeholder = $(thumbnail).next();
 		console.log(currentPos);
 
@@ -40,15 +46,31 @@ $(document).ready(function(){
 
 
 		// Animate panel 3's thumbnail down
+		console.log("thumb 3 is ");
+
 		$('#thumb3-placeholder').css({
-			position: 'absolute'
+			position: 'absolute',
+			top: $('#thumb3').position().top + $('#thumb3').offset().top,
+			left: $('#thumb3').position().left + $('#thumb3').offset().left
 		});
+
 		$('#thumb3-placeholder').show();
 
-
 		$('#thumb3-placeholder').animate({
-			bottom: 0
-		}, 100);
+			top: currentPos.top,
+			left: currentPos.left,
+			width: currentWidth,
+			height: currentHeight,
+			'background-size': currentWidth,
+			opacity: 1,
+			'margin-right': 0
+
+		}, 300, function (){
+
+			$('#thumb3-placeholder').attr('class', 'thumbnail');
+			$('#thumb3-placeholder').attr('id', '');
+
+		});
 
 	}
 
@@ -65,10 +87,15 @@ $(document).ready(function(){
 			'margin-left': '+=450px'
 		}, 500, function(){
 
+			// Rename divs where necessary
 			$('#pane2').attr('id', 'pane3');
 			$('#thumb2').attr('id', 'thumb3');
+			$('#thumb2-placeholder').attr('id', 'thumb3-placeholder');
+
 			$('#pane1').attr('id', 'pane2');
 			$('#thumb1').attr('id', 'thumb2');
+			$('#thumb1-placeholder').attr('id', 'thumb2-placeholder');
+
 			addNewPane();
 
 			$('#pane2').animate({
@@ -81,16 +108,14 @@ $(document).ready(function(){
 		});
 	}
 
-
-
 	function addNewPane(){
-
-
 		$('#panes-container').prepend('<div class="pane" style="opacity: 0" id="pane1"><div class="thumbnail" id="thumb1"></div><div class="description">Here is a description for pane1.</div></div>');
-		
 		$('#pane1').animate({
 			opacity: '1'
-		}, 100);
+		}, 100, function(){
+
+			permitted = true;
+		});
 	}
 
 });
